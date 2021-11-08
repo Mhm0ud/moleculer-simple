a102af0c29ea945dbfae5b5545309cf447f34ceb
Mhm0ud <m.h.m.almasri@hotmail.com>
2021-11-08 16:11:11 +0300

    update services

--- services/api.service.ts
+++ services/api.service.ts
@@ -1,13 +1,11 @@
 import { Service, Context } from 'moleculer';
 import { Service as DService, Action, Method } from 'moleculer-decorators';
 import ApiGateway from 'moleculer-web';
-
-import { OpenApiMixin } from '../utilities/mixins';
 import { IncomingRequest } from '../utilities/types';
 
 @DService({
   name: 'api',
-  mixins: [ApiGateway, OpenApiMixin()],
+  mixins: [ApiGateway],
   // More info about settings: https://moleculer.services/docs/0.14/moleculer-web.html
   settings: {
     port: process.env.PORT || 3000,
@@ -142,38 +140,6 @@ export default class APIService extends Service {
    * @param {IncomingRequest} req
    * @returns {Promise}
    */
-  @Method
-  async authenticate(
-    ctx: Context,
-    route: any,
-    req: IncomingRequest
-  ): Promise<any> {
-    // Read the token from header
-    const auth = req.headers.authorization;
-    if (auth && auth.startsWith('Bearer')) {
-      const token = auth.slice(7);
-      // Check the token. Tip: call a service which verify the token. E.g. `accounts.resolveToken`
-      if (token === '123456') {
-        // Returns the resolved user. It will be set to the `ctx.meta.user`
-        return {
-          id: 1,
-          name: 'John Doe',
-        };
-      } else {
-        // Invalid token
-        throw new ApiGateway.Errors.UnAuthorizedError(
-          ApiGateway.Errors.ERR_INVALID_TOKEN,
-          {
-            error: 'Invalid Token',
-          }
-        );
-      }
-    } else {
-      // No token. Throw an error or do nothing if anonymous access is allowed.
-      // Throw new E.UnAuthorizedError(E.ERR_NO_TOKEN);
-      return null;
-    }
-  }
 
   /**
    * Authorize the request. Check that the authenticated user has right to access the resource.
@@ -185,24 +151,4 @@ export default class APIService extends Service {
    * @param {IncomingRequest} req
    * @returns {Promise}
    */
-  @Method
-  async authorize(
-    ctx: Context<
-      any,
-      {
-        user: string;
-      }
-    >,
-    route: { [key: string]: undefined },
-    req: IncomingRequest
-  ): Promise<any> {
-    // Get the authenticated user.
-    const user = ctx.meta.user;
-    // It check the `auth` property in action schema.
-    if (req.$action.auth === 'required' && !user) {
-      throw new ApiGateway.Errors.UnAuthorizedError('NO_RIGHTS', {
-        error: 'Unauthorized',
-      });
-    }
-  }
 }
--- services/products.service.ts
+++ services/products.service.ts
@@ -1,59 +1,10 @@
 import { Service, Context, Errors, GenericObject } from 'moleculer';
 import { Service as DService, Action, Method } from 'moleculer-decorators';
 
-import { ProductsOpenapi, ProductsValidation } from '../utilities/mixins';
-import { ProductModel, ProductDocument } from '../utilities/models';
-import DbService from '../utilities/mixins/mongo.mixin';
-import { Product, MetaParams } from '../utilities/types';
-import { MyError } from '../utilities/adapters';
-
-const { MoleculerClientError, ValidationError } = Errors;
-
 @DService({
   name: 'products',
-  mixins: [
-    new DbService('Products').start<ProductDocument>(ProductModel),
-    ProductsValidation,
-    ProductsOpenapi,
-  ],
-
-  /**
-   * Default settings
-   */
-  settings: {
-    fields: ['_id', 'name', 'category', 'price'],
-  },
 })
 export default class ProductsService extends Service {
-  /**
-   * Create a new entity.
-   * Auth is required!
-   *
-   * @actions
-   * @param {Object} products - Product entity
-   *
-   * @returns {Object} Created entity
-   */
-  @Action({
-    auth: ['Basic'],
-    visibility: 'published',
-  })
-  create(ctx: Context<Product>): Promise<{ product: Product }> {
-    const product = ctx.params;
-
-    return this._create(ctx, product)
-      .then((entity: Product) => ({
-        product: this.transformResultEntity(entity),
-      }))
-      .catch((err: MyError) => {
-        if (err.name === 'MongoError' && err.code === 11000) {
-          throw new ValidationError(err.message);
-        }
-
-        throw new ValidationError(err.toString());
-      });
-  }
-
   /**
    * Update an entity.
    * Auth is required!
@@ -66,23 +17,8 @@ export default class ProductsService extends Service {
     auth: ['Basic', 'Bearer'],
     visibility: 'published',
   })
-  update(
-    ctx: Context<Partial<Product>, MetaParams<Product>>
-  ): Promise<{ product: Product }> {
-    const product = ctx.params;
-
-    // Bearer only update his entity
-    if (
-      product._id &&
-      ctx.meta.user._id &&
-      product?._id !== ctx.meta.user._id
-    ) {
-      throw new MoleculerClientError("Can't update this entity", 401);
-    }
-
-    return this._update(ctx, product).then((entity: Product) => ({
-      product: this.transformResultEntity(entity),
-    }));
+  update() {
+    return 'products.update';
   }
 
   /**
@@ -94,14 +30,11 @@ export default class ProductsService extends Service {
    * @returns {Object} List of entities
    */
   @Action({
-    auth: ['Basic'],
+    auth: false,
     visibility: 'published',
   })
-  list(ctx: Context<any>) {
-    if (ctx.params.search && !ctx.params.searchFields)
-      ctx.params.searchFields = 'name';
-    const params = this.sanitizeParams(ctx, ctx.params);
-    return this._list(ctx, params).then(this.transformResultList);
+  list() {
+    return 'products.list';
   }
 
   /**
@@ -114,73 +47,10 @@ export default class ProductsService extends Service {
    * @returns {Object} The entity
    */
   @Action({
-    auth: ['Basic', 'Bearer'],
+    auth: false,
     visibility: 'published',
-    cache: {
-      keys: ['id', '#authType'],
-      ttl: 60 * 10,
-    },
   })
-  get(
-    ctx: Context<{ id: string }, MetaParams<Product>>
-  ): Promise<{ product: Product }> {
-    const { id } = ctx.params;
-
-    // Bearer only get his entity
-    if (ctx.meta.user._id && id !== ctx.meta.user._id) {
-      throw new MoleculerClientError("Can't update this entity", 401);
-    }
-
-    return this._get(ctx, { id }).then((entity: Product) => ({
-      product: this.transformResultEntity(entity),
-    }));
-  }
-
-  /**
-   * Transform the result entities
-   *
-   * @param {Array} entities
-   */
-  @Method
-  transformResultList<T>({ rows, ...props }: GenericObject) {
-    const products = rows
-      .map(this.transformResultEntity)
-      .filter((result: T[]) => result);
-
-    return { products, ...props };
-  }
-
-  /**
-   * Transform a result entity
-   *
-   * @param {Context} ctx
-   * @param {Object} entity
-   */
-  @Method
-  transformResultEntity(entity: Product) {
-    if (!entity) return false;
-    return this.sanitizeObject({
-      _id: entity._id,
-      category: entity.category,
-      price: entity.price,
-    });
-  }
-
-  /* Filter null and undefined values
-   *
-   * @param {Object} object
-   */
-  @Method
-  sanitizeObject(object: Partial<Product>) {
-    return Object.entries(object).reduce(
-      (acc, [key, val]) =>
-        val === null || val === undefined
-          ? acc
-          : {
-              ...acc,
-              [key]: val,
-            },
-      {}
-    );
+  get() {
+    return 'products.get';
   }
 }
